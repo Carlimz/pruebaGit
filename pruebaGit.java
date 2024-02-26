8a67c18a810680dad91560f6f0edd99d91981c58
mailenjuarez1 <93502464+mailenjuarez1@users.noreply.github.com>
2024-02-25 21:50:23 -0300

    prueba de branch

--- pruebaGit.java
+++ pruebaGit.java
@@ -1,6 +1,6 @@
 public class pruebaGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bienvenidos al Bootcamp JAVA!");
     }
 }
